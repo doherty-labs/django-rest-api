8938614a9fcc780e319f695534af3bae41804f53
John Doherty <jjdoherty98@gmail.com>
2024-10-17 09:30:56 +0000

    gevent

--- infrastructure/celery-worker/entry.sh
+++ infrastructure/celery-worker/entry.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 cd ./django
-exec celery -A django_project worker --loglevel=debug --scheduler django_celery_beat.schedulers:DatabaseScheduler
+exec celery -A django_project worker -P gevent --loglevel=debug --scheduler django_celery_beat.schedulers:DatabaseScheduler
