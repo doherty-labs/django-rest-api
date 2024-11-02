8e54d3def91e8ec2fd3072e874d0a02233898436
John Doherty <John-Doherty01@users.noreply.github.com>
2024-11-02 21:35:17 +0000

    Check superuser exists (#17)

---
+++ api/django_project/rest_api/management/commands/createadmin.py
@@ -0,0 +1,20 @@
+import os
+
+from django.contrib.auth.management.commands import createsuperuser
+
+
+class Command(createsuperuser.Command):
+    help = "Create a superuser"
+
+    def handle(self, *args, **options):
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+
+        # check if user already exists
+        if self.UserModel.objects.filter(username=username).exists():
+            self.stdout.write(
+                self.style.WARNING(f"Superuser {username} already exists")
+            )
+            return
+
+        # create superuser
+        return super().handle(*args, **options)
