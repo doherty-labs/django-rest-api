be808208d541e52563fe9b6eb9aedbe4b9fd0598
John Doherty <John-Doherty01@users.noreply.github.com>
2024-12-27 16:17:13 +0000

    Seperate websocket service (#35)
    * Check superuser exists
    
    * lint
    
    * dir
    
    * dir
    
    * Pkgs
    
    * Openssl
    
    * Metadata env variables
    
    * Cleanup
    
    * websocket only traffic
    
    * readme
    
    * Daphne cleanup
    
    * Cleanup

--- infrastructure/rest-api/entry.sh
+++ infrastructure/rest-api/entry.sh
@@ -3,14 +3,8 @@ set -e
 cd ./django
 rm -rf ./staticfiles
 
-if [ "$DEBUG_MODE" = "True" ]; then
-    python manage.py collectstatic --noinput
-    python manage.py migrate
-    exec python manage.py runserver 0.0.0.0:8000
-fi
-
 if [ "$ASYNC_MODE" = "True" ]; then
-    exec gunicorn django_project.asgi:application -k uvicorn_worker.UvicornWorker --bind 0.0.0.0:8000 --workers $GUNICORN_WORKERS
+    exec gunicorn django_project.asgi:application -k uvicorn_worker.UvicornWorker --bind 0.0.0.0:8000 --workers $GUNICORN_WORKERS --reload
 fi
 
-exec gunicorn django_project.wsgi:application --bind 0.0.0.0:8000 --workers $GUNICORN_WORKERS --worker-class gevent
+exec gunicorn django_project.wsgi:application --bind 0.0.0.0:8000 --workers $GUNICORN_WORKERS --worker-class gevent --reload
